07f0b664212fc4a1ac9ccb60fe61666ae38a6dfa
grcarmenaty <44200757+grcarmenaty@users.noreply.github.com>
2023-11-29 00:40:39 +0100

    updated nodejs install

--- lxc/nginx-proxy-manager/install/debian.sh
+++ lxc/nginx-proxy-manager/install/debian.sh
@@ -99,7 +99,11 @@ runcmd apt-get update && apt-get install -y -q --no-install-recommends openresty
 
 # Install nodejs
 log "Installing nodejs"
-runcmd wget -qO - https://deb.nodesource.com/setup_16.x | bash -
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_16.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 runcmd apt-get install -y -q --no-install-recommends nodejs
 runcmd npm install --global yarn
 
